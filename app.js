5e4ae952a427dda4e64bb4eac2eafb7d51677ac2
Arvid Alm <arvid.alm@iths.se>
2022-09-19 19:34:45 +0200

    Slamsug
    version2

---
+++ app.js
@@ -0,0 +1,10 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName ('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
+
+/*Jag får absolut ingen Javascript att fungera och har ingen aning varför :) */
+
+/*Help me Richard-Wan Kenobi, you're my only hope!*/
